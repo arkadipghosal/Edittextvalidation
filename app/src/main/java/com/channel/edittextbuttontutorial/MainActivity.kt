9c2dff35f431eb0e5286d3e410db7f3c39b1ab00
Arkadip-Redapple <arkadip@redappletech.com>
2020-09-19 20:26:07 +0530

    chnges in design

--- app/src/main/java/com/channel/edittextbuttontutorial/MainActivity.kt
+++ app/src/main/java/com/channel/edittextbuttontutorial/MainActivity.kt
@@ -2,12 +2,15 @@ package com.channel.edittextbuttontutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar()?.hide();
         setContentView(R.layout.activity_main)
 
 
